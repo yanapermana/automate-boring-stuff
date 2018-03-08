de1feec529f297cf497c50cadc1b56c27f1dec41
yanapermana <yanapermana@github.com>
2018-03-08 10:59:36 +0700

    Whatever

--- kai-id/train_schedule.py
+++ kai-id/train_schedule.py
@@ -1,11 +1,11 @@
 import requests
 import datetime
+import time
 from bs4 import BeautifulSoup
 
 def scrap(html_doc):
 	available = []
 	soup = BeautifulSoup(html_doc, 'html.parser')
-	# print html_doc
 	table = soup.findAll("table", {"class" : "itReservationTable" })[0]
 	for item in table.findAll('tr'):
 		if 'HARINA' in item.text:
@@ -13,13 +13,13 @@ def scrap(html_doc):
 				available.append(item.text)
 	return available
 
-def job():
-	_date = datetime.datetime.now().strftime('%d-%B-%Y')
-	_date_int = datetime.datetime.now().strftime("%Y%m%d")
+def job(_departure_date):
+	_tmp = datetime.datetime.strptime(_departure_date, "%d-%m-%Y")
+	_date = _tmp.strftime('%d-%B-%Y')
+	_date_int = _tmp.strftime("%Y%m%d")
 	url = 'https://kai.id/'
 	s = requests.Session()
 	r = s.get(url)
-	# print r.cookies['csrf_cookie_kai_id']
 	data = {
 		'csrf_kai_id_cookies': r.cookies['csrf_cookie_kai_id'],
 		'origination': 'BD',
@@ -30,7 +30,6 @@ def job():
 		'infant': 0,
 	}
 	url = 'https://kai.id/train_schedule'
-	# print data, _date_int, _date
 	result = s.post(url=url, data=data)
 	body = scrap(result.content)
 	if body:
@@ -38,4 +37,8 @@ def job():
 			print x
 
 if __name__ == '__main__':
-	job()
+	_important_dates = ['29-03-2018','31-05-2018','09-06-2018','11-06-2018']
+	_important_dates = ['29-03-2018','31-05-2018']
+	for _x in _important_dates:
+		job(_x)
+		time.sleep(1)
